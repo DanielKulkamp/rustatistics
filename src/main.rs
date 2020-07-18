afc35889bb20dfd50704c63d9291563fba1cb83c
Daniel Clemes Kulkamp <daniel.kulkamp@gmail.com>
2020-07-18 20:31:28 -0300

    melhor

--- src/main.rs
+++ src/main.rs
@@ -3,39 +3,45 @@ use rand::Rng;
 
 fn main() {
     
-    println!("Generating 100 numbers");
-    let mut numbers : [i32; 100] = [0; 100];
-    
-    print!("[ ");
-    for i in numbers.iter_mut(){
-    	*i = rand::thread_rng().gen_range(1,101);
-    	print!("{:?}, ", i);
-    }
-    println!("]");
+    println!("Please enter how many numbers do you want to generate: ");
+	let mut input = String::new();
+	let count : u32;
+	loop {
+		match std::io::stdin().read_line(&mut input) {
+			Err(_) => {
+				println!("Error reading line");
 
-    let mut sum = 0i32;
-    for i in numbers.iter(){
-    	sum += *i;
-    }
- 	println!("The sum is: {:?}", sum);
- 	let average : f32;
- 	let sum = sum as f32;
- 	average = sum/100f32;
- 	println!("The average is: {:?}", average);
+				continue;
+			},
+			Ok(_) => {
+				count = match input.trim().parse() {
+					Err(_) => {
+						println!("Enter a valid number");
+						input.clear();
+						continue;
+					},
+					Ok(num) => num,
+				}
+			}
+		}
+		break;
+	}
+	let numbers = generate_numbers(count);
+
+	println!("The sum is: {:?}", sum(&numbers));
+
+ 	println!("The mean is: {:?}", mean(&numbers));
+ 	
+	println!("The Median is {:?}", median(&numbers));
  	
 
- 	
- 	numbers.sort();
  	let mut mode_map = HashMap::new();
  	for i in numbers.iter(){
- 		let count = mode_map.entry(i).or_insert(0);
- 		*count += 1;
+ 		let occurences = mode_map.entry(i).or_insert(0);
+ 		*occurences += 1;
  	}
 
- 	let mean : f32 = (numbers[49] as f32 + numbers[50] as f32)/2f32;
-
- 	println!("The Mean is {:?}", mean );
- 	
+	
  	let mut mode = 0;
  	let mut max_count = 0;
  	for (key, val) in mode_map.iter(){
@@ -46,5 +52,51 @@ fn main() {
  	}
  	println!("The mode is {:?} ({:?} times)", mode, max_count);
 
+}
 
+fn generate_numbers(count : u32 ) -> Vec<i32>{
+	let mut numbers : Vec<i32> = Vec::new();
+	println!("Generating {} numbers", count);
+	for _ in 0..count {
+		numbers.push(rand::thread_rng().gen_range(1,101));	
+	}
+    numbers
+}
+
+fn sum(numbers : &Vec<i32> ) -> i32 {
+	let mut sum = 0i32;
+    for i in numbers.iter(){
+    	sum += *i;
+    } 	
+    sum
+}
+
+fn mean(numbers: &Vec<i32>) -> f32 {
+	let average : f32;
+ 	let sum = sum(&numbers) as f32;
+ 	average = sum/( numbers.len() as f32 );
+ 	average
+}
+
+fn median(numbers: &Vec<i32>) -> f32 {
+	let mut sorted : Vec<i32> = numbers.clone();
+	sorted.sort();
+	print_numbers(&sorted);
+	match sorted.len() % 2 {
+		0 => {
+			println!("Len/2-1 {:?}, len/2 {:?}", sorted[sorted.len()/2 -1], sorted[sorted.len()/2] );
+			(sorted[sorted.len()/2 - 1] as f32 + sorted[sorted.len()/2] as f32) / 2f32
+		},
+		_ => {
+			sorted[sorted.len()/2] as f32
+		}
+	}
+}
+
+fn print_numbers(numbers : &Vec<i32>) {
+	print!("[");
+	for number in numbers.iter(){
+		print!(" {}", number)
+	}	
+	println!("]");
 }
